1bce9fe42d99fa0eaeb2489ca6034b369ae13b14
Amy Wilder <henrythepony@gmail.com>
2026-02-13 17:00:27 -0500

    Enable direction buttons for group creation

--- src/jls/elem/Group.java
+++ src/jls/elem/Group.java
@@ -137,6 +137,14 @@ public abstract class Group extends LogicElement {
 			{
 				orientation = JLSInfo.Orientation.RIGHT;
 			}
+			else if(value.equals("UP"))
+			{
+				orientation = JLSInfo.Orientation.UP;
+			}
+			else if(value.equals("DOWN"))
+			{
+				orientation = JLSInfo.Orientation.DOWN;
+			}
 		} else if(name.equals("noncontig")) {
 			if(value.equals("true")) noncontig = true;
 			else noncontig = false;
@@ -144,6 +152,90 @@ public abstract class Group extends LogicElement {
 			super.setValue(name,value);
 		}
 	} // end of setValue method
+
+	/**
+	 *  This method will rotate the group if it is rotate-able.
+	 * @param direction The direction to rotate
+	 * @param g The current graphics context for use in recalculating size
+	 */
+	public void rotate(JLSInfo.Orientation direction, Graphics g)
+	{
+		if (orientation == JLSInfo.Orientation.LEFT)
+		{
+			if(direction == JLSInfo.Orientation.LEFT)
+			{
+				orientation = JLSInfo.Orientation.DOWN;
+			}
+			else
+			{
+				orientation = JLSInfo.Orientation.UP;
+			}
+		}
+		else if (orientation == JLSInfo.Orientation.RIGHT)
+		{
+			if(direction == JLSInfo.Orientation.LEFT)
+			{
+				orientation = JLSInfo.Orientation.UP;
+			}
+			else
+			{
+				orientation = JLSInfo.Orientation.DOWN;
+			}
+		}
+		else if (orientation == JLSInfo.Orientation.UP)
+		{
+			if(direction == JLSInfo.Orientation.LEFT)
+			{
+				orientation = JLSInfo.Orientation.LEFT;
+			}
+			else
+			{
+				orientation = JLSInfo.Orientation.RIGHT;
+			}
+		}
+		else if (orientation == JLSInfo.Orientation.DOWN)
+		{
+			if(direction == JLSInfo.Orientation.LEFT)
+			{
+				orientation = JLSInfo.Orientation.RIGHT;
+			}
+			else
+			{
+				orientation = JLSInfo.Orientation.LEFT;
+			}
+		}
+		width = 0;
+		height = 0;
+		inputs.clear();
+		outputs.clear();
+		init(g);
+	}
+	
+	/**
+	 * Tells if a group is capable of rotating, can only rotate when inputs or outputs have no attachments.
+	 * @return False if any input or output has a wire attached, True otherwise
+	 */
+	public boolean canRotate()
+	{
+		boolean success = true;
+		for(Input i : inputs)
+		{
+			if(i.isAttached())
+			{
+				success = false;
+				break;
+			}
+		}
+		for(Output o : outputs)
+		{
+			if(o.isAttached())
+			{
+				success = false;
+				break;
+			}
+		}
+		return success;
+	}
 	
 	/**
 	 * This method will flip a group
@@ -158,6 +250,14 @@ public abstract class Group extends LogicElement {
 		else if(orientation == JLSInfo.Orientation.RIGHT)
 		{
 			orientation = JLSInfo.Orientation.LEFT;
+		}
+		else if(orientation == JLSInfo.Orientation.UP)
+		{
+			orientation = JLSInfo.Orientation.DOWN;
+		}
+		else if(orientation == JLSInfo.Orientation.DOWN)
+		{
+			orientation = JLSInfo.Orientation.UP;
 		}
 		inputs.clear();
 		outputs.clear();
@@ -286,6 +386,8 @@ public abstract class Group extends LogicElement {
 		private JRadioButton single = new JRadioButton("Single Bits");
 		private JRadioButton group = new JRadioButton("Group Bits");
 		private JRadioButton left = new JRadioButton("Left");
+		private JRadioButton up = new JRadioButton("up");
+		private JRadioButton down = new JRadioButton("down");
 		private JRadioButton right = new JRadioButton("Right", true);
 		private String type;
 		
@@ -339,12 +441,21 @@ public abstract class Group extends LogicElement {
 			JLabel olbl = new JLabel("Orientation");
 			olbl.setAlignmentX(Component.CENTER_ALIGNMENT);
 			window.add(olbl);
-			JPanel orients = new JPanel(new GridLayout(1,3));
+			JPanel orients = new JPanel(new GridLayout(3,3));
+			
+			orients.add(new JLabel(""));
+			orients.add(up);
+			orients.add(new JLabel(""));
 			orients.add(left);
 			orients.add(new JLabel(""));
 			orients.add(right);
+			orients.add(new JLabel(""));
+			orients.add(down);
+			orients.add(new JLabel(""));
 			left.setHorizontalAlignment(SwingConstants.CENTER);
 			right.setHorizontalAlignment(SwingConstants.CENTER);
+			up.setHorizontalAlignment(SwingConstants.CENTER);
+			down.setHorizontalAlignment(SwingConstants.CENTER);
 			ButtonGroup gr = new ButtonGroup();
 			gr.add(left);
 			gr.add(right);
@@ -435,6 +546,14 @@ public abstract class Group extends LogicElement {
 				else if(right.isSelected())
 				{
 					orientation = JLSInfo.Orientation.RIGHT;
+				}
+				else if (up.isSelected())
+				{
+					orientation = JLSInfo.Orientation.UP;
+				}
+				else
+				{
+					orientation = JLSInfo.Orientation.DOWN;
 				}
 				dispose();
 			}
